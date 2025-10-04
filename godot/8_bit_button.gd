d521c82835256a2386c5ac4ae645e1f1203ee2d1
Felipe Verol <felipeverolrocha@gmail.com>
2025-10-04 16:13:21 -0300

    feat: menu finalizado

--- godot/8_bit_button.gd
+++
@@ -1,8 +0,0 @@
-extends TextureButton
-
-@export var label_text: String = "Start"
-@onready var label = $Label
-
-func _ready():
-	if label:
-		label.text = label_text
--- godot/main_menu.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-
-func _on_eight_bit_button_pressed() -> void:
-	print("hello world")
