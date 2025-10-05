7c6a37cedb25cabf1309be8ca593365afefe19b3
Felipe Verol <felipeverolrocha@gmail.com>
2025-10-05 11:35:46 -0300

    fix: farmer game finalizado

--- godot/scripts/alice.gd
+++ godot/scripts/alice.gd
@@ -2,33 +2,38 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 const SPEED_TRATOR = 50.0
+var enable = false
 var last_direction = "down"
 @onready var animation_player = $AnimationPlayer
 
 func _physics_process(delta: float) -> void:
-	var direction = Vector2.ZERO
+	if enable == true:
+		var direction = Vector2.ZERO
 
-	if Input.is_action_pressed("ui_up"):
-		direction = Vector2.UP
-		last_direction = "up"
-	elif Input.is_action_pressed("ui_down"):
-		direction = Vector2.DOWN
-		last_direction = "down"
-	elif Input.is_action_pressed("ui_left"):
-		direction = Vector2.LEFT
-		last_direction = "left"
-	elif Input.is_action_pressed("ui_right"):
-		direction = Vector2.RIGHT
-		last_direction = "right"
+		if Input.is_action_pressed("ui_up"):
+			direction = Vector2.UP
+			last_direction = "up"
+		elif Input.is_action_pressed("ui_down"):
+			direction = Vector2.DOWN
+			last_direction = "down"
+		elif Input.is_action_pressed("ui_left"):
+			direction = Vector2.LEFT
+			last_direction = "left"
+		elif Input.is_action_pressed("ui_right"):
+			direction = Vector2.RIGHT
+			last_direction = "right"
 
-	if direction != Vector2.ZERO:
-		velocity = direction * SPEED
-		animation_player.play(last_direction)
-	else:
-		velocity = Vector2.ZERO
-		animation_player.play("idle_" + last_direction)
+		if direction != Vector2.ZERO:
+			velocity = direction * SPEED
+			animation_player.play(last_direction)
+		else:
+			velocity = Vector2.ZERO
+			animation_player.play("idle_" + last_direction)
+			
+		move_and_slide()
 		
-	move_and_slide()
+func set_enable_true():
+	enable = true
 
 func desativar():
 	queue_free() # Esconde o nó do jogador e seus filhos
